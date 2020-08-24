b4137cb93da23daa74ee978ad9090a950d6ff9cf
Jacobo José Guijarro Villalba <56597297+JacoboGuijar@users.noreply.github.com>
2020-08-24 13:23:04 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,2 @@
 
 console.log("background running...");
-
-// chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
-// 	if (request.message === 'fetch_pls'){
-// 		fetch('https://www.justwatch.com/es/pelicula/origen')
-// 			.then(function (response){
-// 				return response.text();
-// 			})
-// 			.then(function (html){
-// 				console.log(html);
-// 			})
-// 			.catch(function (err){
-// 				 console.log("could not fetch " + err);			
-// 			})
-// 	}
-// });
